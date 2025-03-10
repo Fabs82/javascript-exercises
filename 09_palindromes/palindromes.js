704772013356f76eeac8606d1c88a4df470d3b50
Fabs82 <148846635+Fabs82@users.noreply.github.com>
2025-03-10 10:25:48 +0100

    Solve exercise using split/join/reverse methods

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    formattedWord = word.toLowerCase().replace(/[^a-z0-9]/g, '');
+    reversedWord = formattedWord.split("").reverse().join("");
+    return formattedWord === reversedWord;
 };
 
 // Do not edit below this line
