8e6716755863fd5ce69bbf695430a294820c430b
Fabs82 <148846635+Fabs82@users.noreply.github.com>
2025-03-12 10:19:08 +0100

    Solve fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function (number) {
+    let firstN = 0;
+    let secondN = 1;
+    let myArray = [];
 
+    if (number == 0) return 0;
+
+    else if (number < 0) return "OOPS";
+
+    else {
+        for (let i = 0; i < number; i++) {
+            let newN = firstN + secondN;
+            secondN = firstN;
+            firstN = newN;
+            myArray.push(newN);
+        };
+        return myArray[number - 1]
+    };
 };
 
 // Do not edit below this line
