670d0be36946031ea09c7981932ff1b776173295
Fabs82 <148846635+Fabs82@users.noreply.github.com>
2025-03-13 09:26:05 +0100

    Solve exeercise using forEach and map methods

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,13 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (books) {
+    titles = [];
+    books.forEach(element => { titles.push(element.title) });
+    return titles
 };
 
 // Do not edit below this line
 module.exports = getTheTitles;
+
+// ALTERNATIVE AND BETTER SOLUTION
+//const getTheTitles = function (array) {
+//return array.map((book) => book.title);
+//};
